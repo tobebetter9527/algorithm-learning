c7cd6e806ea22b710b75d386e2c468c1a54a416a
tobebetter9527 <wiljz@qq.com>
2021-08-26 12:30:27 +0800

    leetcode股票最大利润问题

--- algorithm/src/main/java/com/freedom/algorithm/leetcode/MaxProfitSolution.java
+++ algorithm/src/main/java/com/freedom/algorithm/leetcode/MaxProfitSolution.java
@@ -34,23 +34,30 @@ package com.freedom.algorithm.leetcode;
  */
 public class MaxProfitSolution {
 
+  /**
+   * 思路：低买高卖
+   */
   public static int maxProfit(int[] nums) {
     if (nums == null || nums.length < 2) {
       return 0;
     }
     int temp = 0;
     for (int i = 0; i < nums.length; i++) {
+      // 寻找局部最小值
       while ((i + 1) < nums.length && nums[i] > nums[i + 1]) {
         i++;
       }
+      // 如果数组边界下降趋势，则中断
       if ((i + 1) == nums.length - 1 && nums[i] > nums[i + 1]) {
         break;
       }
       temp -= nums[i];
 
+      // 寻找局部最大值
       while ((i + 1) < nums.length && nums[i] < nums[i + 1]) {
         i++;
       }
+      // 如果数组边界是上升趋势，则取最后一个值卖出，否则就是局部最大值
       if ((i + 1) == nums.length - 1 && nums[i] < nums[i + 1]) {
         temp += nums[i + 1];
       } else {
@@ -62,7 +69,7 @@ public class MaxProfitSolution {
 
 
   public static void main(String[] args) {
-    int[] nums = {7,8,8,8,9};
+    int[] nums = {7, 8, 8, 8, 9, 1, 1, 0};
     System.out.println(maxProfit(nums));
   }
 }
