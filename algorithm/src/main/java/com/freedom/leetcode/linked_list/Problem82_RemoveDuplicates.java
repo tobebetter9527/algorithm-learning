1a8ab3fa3a760b6ebfe52ff709fa205bd9b6d1db
tobebetter9527 <wiljz@qq.com>
2022-06-09 17:31:58 +0800

    linked list

--- algorithm/src/main/java/com/freedom/leetcode/linked_list/Problem82_RemoveDuplicates.java
+++ algorithm/src/main/java/com/freedom/leetcode/linked_list/Problem82_RemoveDuplicates.java
@@ -9,25 +9,68 @@ package com.freedom.leetcode.linked_list;
  */
 public class Problem82_RemoveDuplicates {
 
-  public ListNode deleteDuplicates(ListNode head) {
-    ListNode preHead = new ListNode();
-    ListNode pre = preHead;
-    ListNode cur = head;
-    while (cur.next != null) {
-      ListNode temp = cur;
-      boolean flag = true;
-      while (flag) {
-        if (temp.val != cur.next.val) {
-          flag = false;
+  /**
+   * 官方解法：时间复杂度：O(n)O(n)，空间复杂度：O(1)O(1)
+   *
+   * @param head
+   * @return
+   */
+  public static ListNode deleteDuplicates2(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+    // 哨兵节点，简化代码
+    ListNode preHead = new ListNode(-1, head);
+    ListNode cur = preHead;
+    while (cur.next != null && cur.next.next != null) {
+      // 连续两个节点的值相等
+      if (cur.next.val == cur.next.next.val) {
+        int val = cur.next.val;
+        // 判断接下来的点是不是跟val相等，相等，cur.next不停的后跳
+        while (cur.next != null && cur.next.val == val) {
+          cur.next = cur.next.next;
         }
+      } else {
         cur = cur.next;
       }
+    }
+    return preHead.next;
+  }
 
-      pre.next = cur;
+
+  public static ListNode deleteDuplicates(ListNode head) {
+    if (head == null) {
+      return null;
     }
 
-    pre.next = cur;
+    ListNode preHead = new ListNode(-1, head);
+    ListNode pre = preHead;
+    ListNode cur = head;
+    while (cur != null && cur.next != null) {
+      ListNode temp = pre.next;
+      boolean flag = false;
+      while (cur.next != null) {
+        if (temp.val == cur.next.val) {
+          flag = true;
+          cur = cur.next;
+        } else {
+          break;
+        }
+      }
+      if (flag) {
+        pre.next = cur.next;
+      } else {
+        pre = pre.next;
+      }
+      cur = cur.next;
+    }
     return preHead.next;
+  }
+
+  public static void main(String[] args) {
+    ListNode head = new ListNode(1);
+    ListNode listNode = deleteDuplicates(head);
+    System.out.println(listNode);
   }
 
   private static class ListNode {
