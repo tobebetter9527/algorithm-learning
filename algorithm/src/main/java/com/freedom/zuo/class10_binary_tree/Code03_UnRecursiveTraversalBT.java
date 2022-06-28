038e45b433c7110e8e11b91b89c953422c3b585b
tobebetter9527 <wiljz@qq.com>
2022-06-28 18:21:57 +0800

    binary tree - unrecursive version

---
+++ algorithm/src/main/java/com/freedom/zuo/class10_binary_tree/Code03_UnRecursiveTraversalBT.java
@@ -0,0 +1,68 @@
+package com.freedom.zuo.class10_binary_tree;
+
+import com.freedom.zuo.class10_binary_tree.Code02_RecursiveTraversalBT.Node;
+import java.util.Stack;
+
+public class Code03_UnRecursiveTraversalBT {
+
+
+  public static void preTraverse(Node root) {
+    if (root == null) {
+      return;
+    }
+    // 先放入根节点
+    Stack<Node> stack = new Stack<>();
+    stack.push(root);
+    // 先压入右子树，再压入左子树
+    while (!stack.isEmpty()) {
+      Node pop = stack.pop();
+      System.out.print(pop.val + "->");
+      if (pop.right != null) {
+        stack.push(pop.right);
+      }
+      if (pop.left != null) {
+        stack.push(pop.left);
+      }
+    }
+  }
+
+  public static void inTraverse(Node root) {
+    if (root == null) {
+      return;
+    }
+    Stack<Node> stack = new Stack<>();
+    stack.push(root);
+    //
+
+  }
+
+  public static void main(String[] args) {
+    Node head = new Node(1);
+    head.left = new Node(2);
+    head.right = new Node(3);
+    head.left.left = new Node(4);
+    head.left.right = new Node(5);
+    head.right.left = new Node(6);
+    head.right.right = new Node(7);
+
+    preTraverse(head);
+    System.out.println("========");
+    inTraverse(head);
+    System.out.println("========");
+    postTraverse(head);
+    System.out.println("========");
+
+  }
+
+  private static class Node {
+
+    int val;
+    Node left;
+    Node right;
+
+    public Node(int val) {
+      this.val = val;
+    }
+  }
+
+}
