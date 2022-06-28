0a30b24fd809d386c42d7aeb33249de28fc82dbf
tobebetter9527 <wiljz@qq.com>
2022-06-28 20:29:08 +0800

    binary tree - unrecursive version

--- algorithm/src/main/java/com/freedom/zuo/class10_binary_tree/Code03_UnRecursiveTraversalBT.java
+++ algorithm/src/main/java/com/freedom/zuo/class10_binary_tree/Code03_UnRecursiveTraversalBT.java
@@ -1,6 +1,5 @@
 package com.freedom.zuo.class10_binary_tree;
 
-import com.freedom.zuo.class10_binary_tree.Code02_RecursiveTraversalBT.Node;
 import java.util.Stack;
 
 public class Code03_UnRecursiveTraversalBT {
@@ -31,10 +30,43 @@ public class Code03_UnRecursiveTraversalBT {
       return;
     }
     Stack<Node> stack = new Stack<>();
-    stack.push(root);
-    //
-
+    Node cur = root;
+    while (!stack.isEmpty() || cur != null) {
+      if (cur != null) {
+        stack.push(cur);
+        cur = cur.left;
+      } else {
+        cur = stack.pop();
+        System.out.print(cur.val + "->");
+        cur = cur.right;
+      }
+    }
   }
+
+  public static void postTraverse(Node root) {
+    if (root == null) {
+      return;
+    }
+    Node cur = root;
+
+    Stack<Node> s1 = new Stack<>();
+    Stack<Node> s2 = new Stack<>();
+    s1.push(cur);
+    while (!s1.isEmpty()) {
+      cur = s1.pop();
+      s2.push(cur);
+      if (cur.left != null) {
+        s1.push(cur.left);
+      }
+      if (cur.right != null) {
+        s1.push(cur.right);
+      }
+    }
+    while (!s2.isEmpty()) {
+      System.out.print(s2.pop().val + "->");
+    }
+  }
+
 
   public static void main(String[] args) {
     Node head = new Node(1);
