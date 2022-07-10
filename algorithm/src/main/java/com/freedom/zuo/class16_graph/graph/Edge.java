d4466f48961d611fb708495cb71e2bf55cb20b27
tobebetter9527 <wiljz@qq.com>
2022-07-10 16:26:55 +0800

    graph的表示法

---
+++ algorithm/src/main/java/com/freedom/zuo/class16_graph/graph/Edge.java
@@ -0,0 +1,24 @@
+package com.freedom.zuo.class16_graph.graph;
+
+public class Edge {
+
+	/**
+	 * 权重
+	 */
+	public int weight;
+	/**
+	 * 从哪里开始
+	 */
+	public Node from;
+	/**
+	 * 去哪里
+	 */
+	public Node to;
+
+	public Edge(int weight, Node from, Node to) {
+		this.weight = weight;
+		this.from = from;
+		this.to = to;
+	}
+
+}
---
+++ algorithm/src/main/java/com/freedom/zuo/class16_graph/graph/Graph.java
@@ -0,0 +1,22 @@
+package com.freedom.zuo.class16_graph.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Graph {
+
+  /**
+   * 点的值和点的映射关系
+   */
+  public HashMap<Integer, Node> nodes;
+  /**
+   * 边的结合
+   */
+  public HashSet<Edge> edges;
+
+  public Graph() {
+    nodes = new HashMap<>();
+    edges = new HashSet<>();
+  }
+}
+
