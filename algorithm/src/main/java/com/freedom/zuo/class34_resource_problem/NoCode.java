f51b38f1193605ccebd1151ece5bb03385c1caf1
tobebetter9527 <wiljz@qq.com>
2022-08-09 21:55:30 +0800

    no code

---
+++ algorithm/src/main/java/com/freedom/zuo/class34_resource_problem/NoCode.java
@@ -0,0 +1,13 @@
+package com.freedom.zuo.class34_resource_problem;
+
+/**
+ * 本章并无code，因为资源限制类题目输入需要的条件较多并且真的实现代码量巨大
+ * <p>
+ * 面试中这类题目出现也就和面试官聊解法，不会有代码实现的要求
+ *
+ * @author tobebetter9527
+ * @create 2022/08/09 21:54
+ */
+public class NoCode {
+
+}
