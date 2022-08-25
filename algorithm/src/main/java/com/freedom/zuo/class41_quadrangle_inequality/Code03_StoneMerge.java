77e885e05e3a3968d26a1f11d6eb33e10b1c1a81
tobebetter9527 <wiljz@qq.com>
2022-08-25 21:51:58 +0800

    Code03_StoneMerge

--- algorithm/src/main/java/com/freedom/zuo/class41_quadrangle_inequality/Code03_StoneMerge.java
+++ algorithm/src/main/java/com/freedom/zuo/class41_quadrangle_inequality/Code03_StoneMerge.java
@@ -48,7 +48,6 @@ public class Code03_StoneMerge {
     return next + substract(sums, left, right);
   }
 
-
   private static int[] sum(int[] arr) {
     int[] sums = new int[arr.length + 1];
     for (int i = 0; i < arr.length; i++) {
@@ -61,4 +60,103 @@ public class Code03_StoneMerge {
     return sums[right + 1] - sums[left];
   }
 
+  // ------------------------------------------------------ //
+
+  /**
+   * 动态规划
+   *
+   * @param arr
+   */
+  public static int dp1(int[] arr) {
+    if (arr == null || arr.length < 2) {
+      return 0;
+    }
+    int length = arr.length;
+    int[] sums = sum(arr);
+    int[][] dp = new int[length][length];
+    // 隐藏条件left<=right，所以left > right的格子不用，同时left==right的格子值为0
+    // 从小到上，左到右填空
+    for (int left = length - 2; left >= 0; left--) {
+      for (int right = left + 1; right < length; right++) {
+        int next = Integer.MAX_VALUE;
+        for (int leftEnd = left; leftEnd < right; leftEnd++) {
+          next = Math.min(next, dp[left][leftEnd] + dp[left + 1][right]);
+        }
+        // 假设数组{1，2，6，3），假设（1，2），（6，3）分别合并得到两个数，然后再合并，所得的分数最小，
+        // 分数计算： 3 + 6 + （3 + 6）,所以这里： substract(sums, left, right)
+        dp[left][right] = next + substract(sums, left, right);
+      }
+    }
+
+    return dp[0][length - 1];
+  }
+
+  // ------------------------------------------------------ //
+
+  /**
+   * 动态规划,不等式优化
+   *
+   * @param arr
+   */
+  public static int dp2(int[] arr) {
+    if (arr == null || arr.length < 2) {
+      return 0;
+    }
+    int length = arr.length;
+    int[] sums = sum(arr);
+    int[][] dp = new int[length][length];
+    // 分割的最后位置
+    int[][] best = new int[length][length];
+    // 隐藏条件left<=right，所以left > right的格子不用，同时left==right的格子值为0
+    for (int i = 0; i < length - 1; i++) {
+      // 对角线不用填，填次对角线
+      best[i][i + 1] = i;
+      dp[i][i + 1] = substract(sums, i, i + 1);
+    }
+
+    for (int left = length - 3; left >= 0; left--) {
+      for (int right = left + 2; right < length; right++) {
+        int next = Integer.MAX_VALUE;
+        int choose = -1;
+        // todo 这里还是不是很明白，后续搞懂
+        for (int leftEnd = best[left][right - 1]; leftEnd < best[left + 1][right]; leftEnd++) {
+          int cur = Math.min(next, dp[left][leftEnd] + dp[left + 1][right]);
+          if (cur <= next) {
+            next = cur;
+            choose = leftEnd;
+          }
+        }
+        best[left][right] = choose;
+        dp[left][right] = next + substract(sums, left, right);
+      }
+    }
+
+    return dp[0][length - 1];
+  }
+
+  public static int[] randomArray(int len, int maxValue) {
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++) {
+      arr[i] = (int) (Math.random() * maxValue);
+    }
+    return arr;
+  }
+
+  public static void main(String[] args) {
+    int N = 10;
+    int maxValue = 10;
+    int testTime = 10000;
+    System.out.println("测试开始");
+    for (int i = 0; i < testTime; i++) {
+      int len = (int) (Math.random() * N);
+      int[] arr = randomArray(len, maxValue);
+      int ans1 = min1(arr);
+      int ans2 = dp1(arr);
+      int ans3 = dp2(arr);
+      if (ans1 != ans2 || ans1 != ans3) {
+        System.out.println("Oops!");
+      }
+    }
+    System.out.println("测试结束");
+  }
 }
