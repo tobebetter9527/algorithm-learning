03072ebf04fcf22816ceff15eeeb1df3c42fe52d
tobebetter9527 <wiljz@qq.com>
2022-10-16 10:01:40 +0800

    203. Remove Linked List Elements

--- algorithm/src/main/java/com/freedom/leetcode/linked_list/Problem203_RemoveLinkedListElements.java
+++ algorithm/src/main/java/com/freedom/leetcode/linked_list/Problem203_RemoveLinkedListElements.java
@@ -15,7 +15,7 @@ public class Problem203_RemoveLinkedListElements {
    * @param val
    * @return
    */
-  public ListNode removeElements(ListNode head, int val) {
+  public static ListNode removeElements(ListNode head, int val) {
     if (head == null) {
       return null;
     }
@@ -34,8 +34,54 @@ public class Problem203_RemoveLinkedListElements {
     return preHead.next;
   }
 
+  /**
+   * 更简洁
+   *
+   * @param head
+   * @param val
+   * @return
+   */
+  public static ListNode removeElements2(ListNode head, int val) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode preHead = new ListNode(0, head);
+    ListNode cur = preHead;
+    while (cur.next != null) {
+      if (cur.next.val == val) {
+        cur.next = cur.next.next;
+      } else {
+        cur = cur.next;
+      }
+    }
+
+    return preHead.next;
+  }
+
+  /**
+   * recursion
+   *
+   * @param head
+   * @param val
+   * @return
+   */
+  public static ListNode removeElements3(ListNode head, int val) {
+    if (head == null) {
+      return null;
+    }
+
+    head.next = removeElements3(head.next, val);
+    return head.val == val ? head.next : head;
+  }
+
 
   public static void main(String[] args) {
-    System.out.println("");
+    ListNode node1 = new ListNode(5);
+    ListNode node2 = new ListNode(3,node1);
+    ListNode node3 = new ListNode(6,node2);
+    ListNode node = removeElements3(node3, 3);
+    System.out.println(node);
+
   }
 }
