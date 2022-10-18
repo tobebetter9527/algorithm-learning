be335d37c318283c5b519568d806884ecd7fc774
tobebetter9527 <wiljz@qq.com>
2022-10-18 16:11:16 +0800

    19. Remove Nth Node From End of List

--- algorithm/src/main/java/com/freedom/leetcode/linked_list/Problem19_RemoveNthNodeFromEndOfList.java
+++ algorithm/src/main/java/com/freedom/leetcode/linked_list/Problem19_RemoveNthNodeFromEndOfList.java
@@ -8,6 +8,9 @@ package com.freedom.leetcode.linked_list;
  */
 public class Problem19_RemoveNthNodeFromEndOfList {
 
+  /**
+   * time complexity is O(n), space complexity is O(1).
+   */
   public ListNode removeNthFromEnd(ListNode head, int n) {
     if (head == null) {
       return null;
@@ -28,6 +31,33 @@ public class Problem19_RemoveNthNodeFromEndOfList {
     }
     cur.next = cur.next.next;
     return preHead.next;
+  }
+
+  /**
+   * time complexity is O(n), space complexity is O(1).
+   * <p>
+   * 双指针法
+   */
+  public ListNode removeNthFromEnd2(ListNode head, int n) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode dummyNode = new ListNode(0, head);
+    ListNode fast = dummyNode;
+    ListNode slow = dummyNode;
+
+    for (int i = 0; i < n; i++) {
+      fast = fast.next;
+    }
+
+    while (fast.next != null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+    return dummyNode.next;
   }
 
 
