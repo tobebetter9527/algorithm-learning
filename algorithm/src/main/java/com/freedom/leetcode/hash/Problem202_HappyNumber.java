5e1c1a3081f93b887e20c7e7d9a40b6cd10f032f
tobebetter9527 <wiljz@qq.com>
2022-10-20 00:08:35 +0800

    202. Happy Number

---
+++ algorithm/src/main/java/com/freedom/leetcode/hash/Problem202_HappyNumber.java
@@ -0,0 +1,38 @@
+package com.freedom.leetcode.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 202. Happy Number
+ *
+ * @author tobebetter9527
+ * @create 2022/10/19 23:21
+ */
+public class Problem202_HappyNumber {
+
+  public static boolean isHappy(int n) {
+    Set<Integer> set = new HashSet<>();
+    while (n != 1 && !set.contains(n)) {
+      set.add(n);
+      n = getSum(n);
+    }
+
+    return n == 1;
+  }
+
+  private static int getSum(int n) {
+    int sum = 0;
+    while (n != 0) {
+      int m = n % 10;
+      sum += m * m;
+      n = n / 10;
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isHappy(19));
+  }
+
+}
--- algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
+++ algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
@@ -6,18 +6,8 @@ import java.util.LinkedList;
 public class MyDemo {
 
   public static void main(String[] args) {
-    System.out.println(5 >> 1);
-
-    int[] nums = new int[1];
-    nums[0] = -2;
-    nums[0] = nums[0] ^ 2;
-    System.out.println(nums[0]);
-
-    ArrayList<String> list = new ArrayList<>();
-    list.get(-2);
-    list.add(1, "");
-    list.add("");
-    list.remove(1);
+   int n = 245;
+    System.out.println(n % 10);
 
   }
 
