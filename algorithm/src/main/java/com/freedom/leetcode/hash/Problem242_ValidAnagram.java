60e23307283de8f3211f306fc5cc22e94c3ccfbc
tobebetter9527 <wiljz@qq.com>
2022-10-19 22:09:30 +0800

    242. Valid Anagram

--- algorithm/src/main/java/com/freedom/leetcode/hash/Problem242_ValidAnagram.java
+++ algorithm/src/main/java/com/freedom/leetcode/hash/Problem242_ValidAnagram.java
@@ -1,5 +1,6 @@
 package com.freedom.leetcode.hash;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +19,10 @@ public class Problem242_ValidAnagram {
    * @param t
    */
   public static boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
     int[] arr = new int[26];
 
     for (char c : s.toCharArray()) {
@@ -39,8 +44,28 @@ public class Problem242_ValidAnagram {
     return true;
   }
 
+  /**
+   * time complexity is O(nlogn) + O(n) , 总体是O(nlogn)。
+   *
+   * @param s
+   * @param t
+   * @return
+   */
+  public static boolean isAnagram3(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
+    char[] chars1 = s.toCharArray();
+    char[] chars2 = t.toCharArray();
+    Arrays.sort(chars1);
+    Arrays.sort(chars2);
+    return Arrays.equals(chars1, chars2);
+  }
 
   /**
+   * time complexity is O(n), space complexity is O(m), m is length of s string.
+   * <p>
    * Constraints:
    * <p>
    * 1 <= s.length, t.length <= 5 * 104
@@ -48,6 +73,10 @@ public class Problem242_ValidAnagram {
    * s and t consist of lowercase English letters.
    */
   public static boolean isAnagram2(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
     Map<Character, Integer> map = new HashMap<>();
     for (char c : s.toCharArray()) {
       map.put(c, map.getOrDefault(c, 0) + 1);
