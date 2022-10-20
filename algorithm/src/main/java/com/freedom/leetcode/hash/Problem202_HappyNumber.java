2c191f71f21e2221508426b8976c2695e9f6b27f
tobebetter9527 <wiljz@qq.com>
2022-10-20 20:22:09 +0800

    202. Happy Number

--- algorithm/src/main/java/com/freedom/leetcode/hash/Problem202_HappyNumber.java
+++ algorithm/src/main/java/com/freedom/leetcode/hash/Problem202_HappyNumber.java
@@ -31,6 +31,27 @@ public class Problem202_HappyNumber {
     return sum;
   }
 
+
+  public static boolean isHappy2(int n) {
+    int slow = n;
+    int fast = getNext(n);
+    while (fast != 1 && slow != fast) {
+      slow = getNext(slow);
+      fast = getNext(getNext(fast));
+    }
+    return fast == 1;
+  }
+
+  private static int getNext(int n) {
+    int sum = 0;
+    while (n > 0) {
+      int m = n % 10;
+      sum += m * m;
+      n = n / 10;
+    }
+    return sum;
+  }
+
   public static void main(String[] args) {
     System.out.println(isHappy(19));
   }
