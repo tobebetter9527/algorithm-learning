a1735d2ee5033307890a924484099988bdbbb79d
tobebetter9527 <wiljz@qq.com>
2022-10-22 11:41:58 +0800

    15. 3Sum

--- algorithm/src/main/java/com/freedom/leetcode/hash/Problem15_3Sum.java
+++ algorithm/src/main/java/com/freedom/leetcode/hash/Problem15_3Sum.java
@@ -1,6 +1,7 @@
 package com.freedom.leetcode.hash;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,27 +12,103 @@ import java.util.List;
  */
 public class Problem15_3Sum {
 
+  /**
+   * 排序 + 双指针
+   * <p>
+   * 固定a,然后b,c从两端向中间移动。
+   * <p>
+   * time complextiy is O(n<sup>2</sup>), space complexity is O(logn).
+   *
+   * @param nums
+   * @return
+   */
   public static List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> ans = new ArrayList<>();
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        for (int k = j + 1; k < nums.length; k++) {
-          if (nums[i] + nums[j] + nums[k] == 0) {
-            List<Integer> list = new ArrayList<>();
-            list.add(nums[i]);
-            list.add(nums[j]);
-            list.add(nums[k]);
-            ans.add(list);
+    int n = nums.length;
+    // 数据从小到大排序，那么三个数必须满足a<=b<=c
+    Arrays.sort(nums);
+
+    for (int first = 0; first < n; first++) {
+      // 如果第一个数大于0，不可能有三个数相加等于0，优化点
+      if (nums[first] > 0) {
+        return ans;
+      }
+
+      // 第一层循环，如果当前和之前的数据相等，就跳过，比如-2，-2，1，1，2，2。index为1和0的都为-2，显然第二个-2可以跳过，
+      // 因为已经在第一个-2中枚举过了。
+      if (first > 0 && nums[first] == nums[first - 1]) {
+        continue;
+      }
+      // 第三层循环的起始位置
+      int third = n - 1;
+      int target = -nums[first];
+
+      // second肯定是大于first的
+      for (int second = first + 1; second < n; second++) {
+        // -3,-2,-2,1,1,2. 当idxA=0,idxB=2时，显示idxB=1,已经枚举过了，都是b=-2.
+        if (second > first + 1 && nums[second] == nums[second - 1]) {
+          continue;
+        }
+
+        // c指针从右向左移动
+        while (second < third && nums[second] + nums[third] > target) {
+          third--;
+        }
+        // 如果相等，b指针不能继续向右移动，否则c指针就必须向左移动，这就重复枚举了。
+        if (second == third) {
+          break;
+        }
+
+        if (nums[second] + nums[third] == target) {
+          ans.add(Arrays.asList(nums[first], nums[second], nums[third]));
+        }
+      }
+    }
+
+    return ans;
+  }
+
+
+  public static List<List<Integer>> threeSum2(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      if (nums[i] > 0) {
+        return ans;
+      }
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+
+      int left = i + 1;
+      int right = n - 1;
+      while (right > left) {
+        int sum = nums[i] + nums[left] + nums[right];
+        if (sum > 0) {
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+          while (right > left && nums[right] == nums[right - 1]) {
+            right--;
           }
+          while (right > left && nums[left] == nums[left + 1]) {
+            left++;
+          }
+          left++;
+          right--;
         }
       }
     }
     return ans;
   }
 
+
   public static void main(String[] args) {
     int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
-    List<List<Integer>> lists = threeSum(nums);
+    List<List<Integer>> lists = threeSum2(nums);
     System.out.println(lists);
   }
 }
