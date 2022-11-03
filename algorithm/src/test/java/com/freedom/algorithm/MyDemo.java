65b6bec920b27bab0a3fcbb0aca528e46b73ef97
tobebetter9527 <wiljz@qq.com>
2022-11-03 22:01:19 +0800

    150. Evaluate Reverse Polish Notation

--- algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
+++ algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
@@ -3,8 +3,7 @@ package com.freedom.algorithm;
 public class MyDemo {
 
   public static void main(String[] args) {
-    int m = "我们的人".indexOf("们的45247658");
-    System.out.println(m);
+    System.out.println(6/-132);
 
   }
 
