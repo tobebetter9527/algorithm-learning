8edfddf3d0e8e3f2aa133aa13dc7eeb06c16a556
tobebetter9527 <wiljz@qq.com>
2022-11-05 21:44:59 +0800

    226. Invert Binary Tree

--- algorithm/src/main/java/com/freedom/leetcode/binary_tree/Problem226_InvertBinaryTree.java
+++ algorithm/src/main/java/com/freedom/leetcode/binary_tree/Problem226_InvertBinaryTree.java
@@ -1,5 +1,8 @@
 package com.freedom.leetcode.binary_tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 226. Invert Binary Tree
  *
@@ -27,5 +30,28 @@ public class Problem226_InvertBinaryTree {
   }
 
 
+  public TreeNode invertTree2(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      TreeNode node = queue.poll();
+
+      TreeNode temp = node.left;
+      node.left = node.right;
+      node.right = temp;
+
+      if (node.left != null) {
+        queue.add(node.left);
+      }
+      if (node.right != null) {
+        queue.add(node.right);
+      }
+    }
+    return root;
+  }
 
 }
