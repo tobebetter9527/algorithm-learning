9adf89f7c60984e98907abb92f4fc14d7214042c
tobebetter9527 <wiljz@qq.com>
2022-11-06 21:35:42 +0800

    222. Count Complete Tree Nodes

---
+++ algorithm/src/main/java/com/freedom/leetcode/binary_tree/Problem222_CountCompleteTreeNodes.java
@@ -0,0 +1,54 @@
+package com.freedom.leetcode.binary_tree;
+
+/**
+ * 222. Count Complete Tree Nodes
+ *
+ * @author tobebetter9527
+ * @create 2022/11/06 17:03
+ */
+public class Problem222_CountCompleteTreeNodes {
+
+  /**
+   * time complexity is O(n), space complexity is O(height).
+   *
+   * @param root
+   * @return
+   */
+  public int countNodes(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int left = countNodes(root.left);
+    int right = countNodes(root.right);
+    return left + right + 1;
+  }
+
+
+  /**
+   *
+   * @param root
+   * @return
+   */
+  public int countNodes2(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftDepth = getDepth(root.left);
+    int rightDepth = getDepth(root.right);
+    if (leftDepth == rightDepth) {
+      return (1 << leftDepth) + countNodes2(root.right);
+    } else {
+      return (1 << rightDepth) + countNodes2(root.left);
+    }
+  }
+
+  private int getDepth(TreeNode left) {
+    int depth = 0;
+    while (left != null) {
+      left = left.left;
+      depth++;
+    }
+    return depth;
+  }
+
+}
