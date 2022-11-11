c0b5507efa064b49d52822dc7c4178bd94d97d4f
tobebetter9527 <wiljz@qq.com>
2022-11-11 17:17:51 +0800

    236. Lowest Common Ancestor of a Binary Tree

--- algorithm/src/main/java/com/freedom/leetcode/binary_tree/Problem236_LowestCommonAncestorofaBinaryTree.java
+++ algorithm/src/main/java/com/freedom/leetcode/binary_tree/Problem236_LowestCommonAncestorofaBinaryTree.java
@@ -13,6 +13,53 @@ import java.util.Set;
  */
 public class Problem236_LowestCommonAncestorofaBinaryTree {
 
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    return process(root, p, q).ancestor;
+  }
+
+  private Info process(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null) {
+      return new Info(false, false, null);
+    }
+    Info left = process(root.left, p, q);
+    if (left.isFindP && left.isFindQ) {
+      return left;
+    }
+    Info right = process(root.right, p, q);
+    if (right.isFindP && right.isFindQ) {
+      return right;
+    }
+
+    boolean isFindP = left.isFindP || right.isFindP || root == p;
+    boolean isFindQ = left.isFindQ || right.isFindQ || root == q;
+    return isFindP && isFindQ ? new Info(isFindP, isFindQ, root) : new Info(isFindP, isFindQ, null);
+  }
+
+  static class Info {
+    boolean isFindP;
+    boolean isFindQ;
+    TreeNode ancestor;
+
+    public Info(boolean isFindP, boolean isFindQ, TreeNode ancestor) {
+      this.isFindP = isFindP;
+      this.isFindQ = isFindQ;
+      this.ancestor = ancestor;
+    }
+  }
+
+  public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == p || root == q || root == null) {
+      return root;
+    }
+    TreeNode left = lowestCommonAncestor3(root.left, p, q);
+    TreeNode right = lowestCommonAncestor3(root.right, p, q);
+    if (left != null && right != null) {
+      return root;
+    }
+    return left != null ? left : right;
+  }
+
+
   /**
    * time complexity is O(n), space complexity is O(n).
    *
