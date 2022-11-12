391eadd9d2ae5cf3ec85f06ad1a0d412a9127734
tobebetter9527 <wiljz@qq.com>
2022-11-12 10:56:43 +0800

    450. Delete Node in a BST

---
+++ algorithm/src/main/java/com/freedom/leetcode/binary_tree/Problem450_DeleteNodeinaBST.java
@@ -0,0 +1,60 @@
+package com.freedom.leetcode.binary_tree;
+
+/**
+ * 450. Delete Node in a BST
+ *
+ * @author tobebetter9527
+ * @create 2022/11/11 22:53
+ */
+public class Problem450_DeleteNodeinaBST {
+
+  public static TreeNode deleteNode(TreeNode root, int key) {
+    if (root == null) {
+      return null;
+    }
+    if (root.val < key) {
+      root.right = deleteNode(root.right, key);
+    } else if (root.val > key) {
+      root.left = deleteNode(root.left, key);
+    } else {
+      if (root.left == null && root.right == null) {
+        root = null;
+      } else if (root.left != null && root.right == null) {
+        root = root.left;
+      } else if (root.left == null && root.right != null) {
+        root = root.right;
+      } else {
+        TreeNode cur = root.right;
+        while (cur.left != null) {
+          cur = cur.left;
+        }
+
+        root.right = deleteNode(root.right, cur.val);
+
+        cur.left = root.left;
+        cur.right = root.right;
+        root = cur;
+      }
+    }
+
+    return root;
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(5);
+    TreeNode node1 = new TreeNode(3);
+    TreeNode node2 = new TreeNode(6);
+    TreeNode node3 = new TreeNode(2);
+    TreeNode node4 = new TreeNode(4);
+    TreeNode node5 = new TreeNode(7);
+    root.left = node1;
+    root.right= node2;
+    node1.left = node3;
+    node1.right = node4;
+    node2.right = node5;
+    TreeNode node = deleteNode(root, 3);
+    System.out.println(node.val);
+
+  }
+
+}
