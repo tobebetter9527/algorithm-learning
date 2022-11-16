79cafe6cd35396c121054e3e195fdbaa24d51dd7
tobebetter9527 <wiljz@qq.com>
2022-11-16 17:26:39 +0800

    93. Restore IP Addresses

--- algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
+++ algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
@@ -1,11 +1,56 @@
 package com.freedom.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MyDemo {
 
   public static void main(String[] args) {
-    System.out.println(Integer.MAX_VALUE);
-
+    MyDemo myDemo = new MyDemo();
+    List<List<String>> aaba = myDemo.partition("aaba");
+    System.out.println(aaba);
   }
+
+
+
+  boolean[][] f;
+  List<List<String>> ret = new ArrayList<List<String>>();
+  List<String> ans = new ArrayList<String>();
+  int n;
+
+  public List<List<String>> partition(String s) {
+    n = s.length();
+    f = new boolean[n][n];
+    for (int i = 0; i < n; ++i) {
+      Arrays.fill(f[i], true);
+    }
+
+    for (int i = n - 1; i >= 0; --i) {
+      for (int j = i + 1; j < n; ++j) {
+        f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+      }
+    }
+
+    dfs(s, 0);
+    return ret;
+  }
+
+  public void dfs(String s, int i) {
+    if (i == n) {
+      ret.add(new ArrayList<String>(ans));
+      return;
+    }
+    for (int j = i; j < n; ++j) {
+      if (f[i][j]) {
+        ans.add(s.substring(i, j + 1));
+        dfs(s, j + 1);
+        ans.remove(ans.size() - 1);
+      }
+    }
+  }
+
+
 
 
   
