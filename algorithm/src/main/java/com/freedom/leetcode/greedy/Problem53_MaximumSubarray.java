b1f83e07433c221bd91d0158a21634d62c947561
tobebetter9527 <wiljz@qq.com>
2022-11-22 20:33:39 +0800

    53. Maximum Subarray

---
+++ algorithm/src/main/java/com/freedom/leetcode/greedy/Problem53_MaximumSubarray.java
@@ -0,0 +1,38 @@
+package com.freedom.leetcode.greedy;
+
+/**
+ * 53. Maximum Subarray
+ *
+ * @author tobebetter9527
+ * @create 2022/11/22 20:19
+ */
+public class Problem53_MaximumSubarray {
+
+  /**
+   * time complexity is O(N),sapce complexity is O(1)
+   *
+   * @param nums
+   * @return
+   */
+  public int maxSubArray(int[] nums) {
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      if (sum > max) {
+        max = sum;
+      }
+      if (sum < 0) {
+        sum = 0;
+      }
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    Problem53_MaximumSubarray problem = new Problem53_MaximumSubarray();
+    int i = problem.maxSubArray(nums);
+    System.out.println(i);
+  }
+}
