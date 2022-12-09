ae7beda56ef4fd78fba25ef2c40c8aa5ab9af60a
tobebetter9527 <wiljz@qq.com>
2022-12-09 23:11:09 +0800

    123. Best Time to Buy and Sell Stock III

--- algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
+++ algorithm/src/test/java/com/freedom/algorithm/MyDemo.java
@@ -3,23 +3,10 @@ package com.freedom.algorithm;
 public class MyDemo {
 
   public static void main(String[] args) {
-    String sentence = "leetcode exercises sound delightful";
-    System.out.println(isCircularSentence(sentence));
+
+
+
   }
-
-  public static boolean isCircularSentence(String sentence) {
-    String[] arr = sentence.split(" ");
-    int n = arr.length;
-
-    for (int i = 0; i < n - 1; i++) {
-      if (arr[i].charAt(arr[i].length() - 1) != arr[i + 1].charAt(0)) {
-        return false;
-      }
-    }
-
-    return  arr[0].charAt(0) == arr[n - 1].charAt(arr[n - 1].length() - 1);
-  }
-
 
 
 }
