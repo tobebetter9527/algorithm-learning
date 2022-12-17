9aab430c48bc3ffb82bf7efcbb77324186fbbbc2
tobebetter9527 <wiljz@qq.com>
2022-12-17 15:19:34 +0800

    35. Search Insert Position

---
+++ algorithm/src/main/java/com/freedom/leetcode/array/Problem35_SearchInsertPosition.java
@@ -0,0 +1,31 @@
+package com.freedom.leetcode.array;
+
+/**
+ * 35. Search Insert Position
+ *
+ * @author tobebetter9527
+ * @create 2022/12/17 15:13
+ */
+public class Problem35_SearchInsertPosition {
+
+  public static int searchInsert(int[] nums, int target) {
+    int low = 0, high = nums.length - 1;
+    while (low <= high) {
+      int mid = low + ((high - low) >> 1);
+      if (target < nums[mid]) {
+        high = mid - 1;
+      } else if (target > nums[mid]) {
+        low = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+    return low ;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1, 3, 5, 6};
+    int target = 7;
+    System.out.println(searchInsert(nums, target));
+  }
+}
