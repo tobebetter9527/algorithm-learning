c395892e67a2ea673c33a4c9f82f3b43c8227a5f
tobebetter9527 <wiljz@qq.com>
2022-12-23 11:16:42 +0800

    Offer04

---
+++ algorithm/src/main/java/com/freedom/sword_offer/Offer04.java
@@ -0,0 +1,31 @@
+package com.freedom.sword_offer;
+
+public class Offer04 {
+
+  /**
+   * time complexity is O(n + m), space complexity is (1)
+   *
+   * @param matrix
+   * @param target
+   * @return
+   */
+  public boolean findNumberIn2DArray(int[][] matrix, int target) {
+    int n = matrix.length;
+    if (n == 0) {
+      return false;
+    }
+    int m = matrix[0].length;
+    int i = 0, j = m - 1;
+    while (i < n && j >= 0) {
+      int num = matrix[i][j];
+      if (num > target) {
+        j--;
+      } else if (num < target) {
+        i++;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
