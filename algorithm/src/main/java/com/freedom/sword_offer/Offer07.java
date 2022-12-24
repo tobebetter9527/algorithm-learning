eac04f42ad2f1b9580f7dce5a380bc5bfd7632b8
tobebetter9527 <wiljz@qq.com>
2022-12-24 16:43:20 +0800

    剑指 Offer 07. 重建二叉树

--- algorithm/src/main/java/com/freedom/sword_offer/Offer07.java
+++ algorithm/src/main/java/com/freedom/sword_offer/Offer07.java
@@ -40,9 +40,34 @@ public class Offer07 {
     return root;
   }
 
+  public static TreeNode buildTree2(int[] preorder, int[] inorder) {
+    int n = preorder.length;
+    if (n == 0) {
+      return null;
+    }
+    Map<Integer, Integer> map = new HashMap<>(n);
+    for (int i = 0; i < n; i++) {
+      map.put(inorder[i], i);
+    }
+    return recursive2(map, preorder, 0,0, n - 1);
+  }
+
+  private static TreeNode recursive2(Map<Integer, Integer> map, int[] preorder,int rootIndex, int inLeft, int inRight) {
+    if (inLeft > inRight) {
+      return null;
+    }
+    int rootValue = preorder[rootIndex];
+    int index = map.get(rootValue);
+    TreeNode root = new TreeNode(rootValue);
+    root.left = recursive2(map, preorder, rootIndex + 1, inLeft, index - 1);
+    root.right = recursive2(map, preorder, rootIndex + (index - inLeft) + 1, index + 1, inRight);
+    return root;
+  }
+
+
   public static void main(String[] args) {
     int[] preorder = {1, 2, 3}, inorder = {2, 3, 1};
-    TreeNode treeNode = buildTree(preorder, inorder);
+    TreeNode treeNode = buildTree2(preorder, inorder);
     System.out.println(treeNode.val);
   }
 }
