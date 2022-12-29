18e44afc4cb5472324e31b40f4eb66199131f514
tobebetter9527 <wiljz@qq.com>
2022-12-29 20:07:28 +0800

    剑指 Offer 09. 用两个栈实现队列

---
+++ algorithm/src/main/java/com/freedom/sword_offer/Offer09.java
@@ -0,0 +1,39 @@
+package com.freedom.sword_offer;
+
+import java.util.Stack;
+
+/**
+ * 剑指 Offer 09. 用两个栈实现队列
+ *
+ * @author tobebetter9527
+ * @create 2022/12/25 10:05
+ */
+public class Offer09 {
+
+  class CQueue {
+
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    public CQueue() {
+      in = new Stack<>();
+      out = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+      in.push(value);
+    }
+
+    public int deleteHead() {
+      if (out.isEmpty()) {
+        while (!in.isEmpty()) {
+          out.push(in.pop());
+        }
+      }
+      if (out.isEmpty()) {
+        return -1;
+      }
+      return out.pop();
+    }
+  }
+}
