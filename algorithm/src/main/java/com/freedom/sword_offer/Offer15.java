5634991e24f2c668fa3e2f3ca1cc1ffe7d5798e1
tobebetter9527 <wiljz@qq.com>
2023-02-05 10:22:49 +0800

    offer15

---
+++ algorithm/src/main/java/com/freedom/sword_offer/Offer15.java
@@ -0,0 +1,24 @@
+package com.freedom.sword_offer;
+
+/**
+ * @author tobebetter9527
+ * @create 2023/02/05 10:03
+ */
+public class Offer15 {
+
+  public static int hammingWeight(int n) {
+    int res = 0;
+    for (int i = 0; i < 32; i++) {
+      if ((n >> i & 1) == 1) {
+        res++;
+      }
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    int n = 120;
+    System.out.println(hammingWeight(n));
+
+  }
+}
