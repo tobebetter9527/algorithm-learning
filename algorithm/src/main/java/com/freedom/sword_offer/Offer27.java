d0a4bd20b50e3e1267b8175c187c6230ebc2fc48
tobebetter9527 <wiljz@qq.com>
2023-02-13 20:46:05 +0800

    offer27

---
+++ algorithm/src/main/java/com/freedom/sword_offer/Offer27.java
@@ -0,0 +1,18 @@
+package com.freedom.sword_offer;
+
+/**
+ * @author tobebetter9527
+ * @create 2023/02/13 20:38
+ */
+public class Offer27 {
+
+  public TreeNode mirrorTree(TreeNode root) {
+    if (root != null) {
+      TreeNode left = mirrorTree(root.left);
+      TreeNode right = mirrorTree(root.right);
+      root.left = right;
+      root.right = left;
+    }
+    return root;
+  }
+}
