aaf30d166761cfb3b98673506ccbd393ab9ef4a0
tobebetter9527 <wiljz@qq.com>
2023-03-06 21:09:58 +0800

    Offer68I

---
+++ algorithm/src/main/java/com/freedom/sword_offer/Offer68I.java
@@ -0,0 +1,18 @@
+package com.freedom.sword_offer;
+
+/**
+ * @author tobebetter9527
+ * @create 2023/03/06 21:07
+ */
+public class Offer68I {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root.val > p.val && root.val > q.val) {
+      return lowestCommonAncestor(root.left, p, q);
+    }
+    if (root.val < p.val && root.val < q.val) {
+      return lowestCommonAncestor(root.right, p, q);
+    }
+    return root;
+  }
+}
