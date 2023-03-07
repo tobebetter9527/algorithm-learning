6f65e87d58758bb3d2ba28bf97f3bc3470c5d72e
tobebetter9527 <wiljz@qq.com>
2023-03-07 21:50:50 +0800

    offer35

---
+++ algorithm/src/main/java/com/freedom/sword_offer/Offer65.java
@@ -0,0 +1,17 @@
+package com.freedom.sword_offer;
+
+/**
+ * @author tobebetter9527
+ * @create 2023/03/07 21:48
+ */
+public class Offer65 {
+
+  public int add(int a, int b) {
+    while (b != 0) {
+      int c = (a & b) << 1;
+      a ^= b;
+      b = c;
+    }
+    return a;
+  }
+}
